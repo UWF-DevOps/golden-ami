d59d61838ded538a8c11991a76dd5f4fe6590ddf
Cole Amick <amickcole@gmail.com>
2015-09-11 00:00:36 -0500

    Update application.sh

--- packer-templates/scripts/application.sh
+++ packer-templates/scripts/application.sh
@@ -7,8 +7,9 @@ apt-get upgrade
 touch /tmp/installLog.txt
 yum update -y -q
 adduser glass
-wget -a /tmp/installLog.txt -t 1 -O /var/tmp/jdk.tar.gz http://download.oracle.com/otn-pub/java/java_ee_sdk/7u3/java_ee_sdk-7u1.zip
-wget -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish.zip http://download.oracle.com/otn-pub/java/glassfish/3122/ogs-3.1.2.2.zip
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/jdk.tar.gz http://download.oracle.com/otn-pub/java/java_ee_sdk/7u3/java_ee_sdk-7u1.zip
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish.zip http://download.oracle.com/otn-pub/java/glassfish/3122/ogs-3.1.2.2.zip
+
 #wget -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish https://s3.amazonaws.com/nwilde.uwf.edu/glassfish
 su -l glass
 # now I am in /home/glass
