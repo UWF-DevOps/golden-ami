4ade83672368d09881f2630c3b04cee30331d19a
tcamick <amickcole@gmail.com>
2015-09-17 20:45:45 -0500

    fixing application.sh

--- packer-scripts/scripts/application.sh
+++ packer-scripts/scripts/application.sh
@@ -10,34 +10,38 @@ apt-get install wget -y
 
 echo "glass:glasscannotlogin::::/home/glass/:/usr/sbin/nologin" > glassfish_user.txt
 newusers glassfish_user.txt
-wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/jdk.tar.gz http://download.oracle.com/otn-pub/java/java_ee_sdk/7u3/java_ee_sdk-7u1.zip
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/jdk.zip http://download.oracle.com/otn-pub/java/java_ee_sdk/7u3/java_ee_sdk-7u1.zip
 wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish.zip http://download.oracle.com/otn-pub/java/glassfish/3122/ogs-3.1.2.2.zip
 
-su -s /bin/bash glass
-cd /home/glass
+#Go to glass user's home
+cd /home/glass/
 
+#We only want applications to be run with glass permissions
 #Get application source code
 git clone https://www.github.com/tcamick/golden-ami
+chown -r glass:glass /home/glass/golden-ami
 
-# now I am in /home/glass
-cp /var/tmp/glassfish.zip .
-cp /var/tmp/jdk.tar.gz .
+cp /var/tmp/glassfish.zip /home/glass/glassfish.zip
+cp /var/tmp/jdk.zip /home/glass/jdk.zip
 
-tar -zxvf jdk.tar.gz
-# bin directory is ~glass/jdk1.8.0_51/bin == THIS WILL CHANGE ==
-unzip glassfish.zip
+chown -r glass:glass /home/glass/glassfish.zip
+chown -r glass:glass /home/glass/jdk.zip
+
+
+unzip /home/glass/jdk.tar.gz
+unzip /home/glass/glassfish.zip
 
 # bin directory containing asadmin is ~glass/glassfish4/bin == THIS MAY CHANGE ==
 #Use vim to edit the glass .bash_profile file to put these two bin's BEFORE anything else. Resulting file is:
 echo "
 # User specific environment and startup programs
-PATH=$PATH:$HOME/bin
-PATH=$HOME/glassfish3/bin:$PATH
-PATH=$HOME/jdk1.8.0_51/bin:$PATH
+#PATH=$PATH:/home/glass/bin
+PATH=/home/glass/glassfish3/bin:$PATH
+PATH=/home/glass/jdk1.8.0_51/bin:$PATH
 export PATH
 " >> /home/glass/.bash_profile
 # back to root
-exit
+
 # set glassfish to run on startup
 cp /home/glass/golden-ami/packer-scripts/scripts/glassfish.sh /etc/init.d/glassfish.sh
 chmod a+rx /etc/init.d/glassfish.sh
